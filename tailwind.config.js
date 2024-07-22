573bf802ad462e5a71fe4a8304cb188de132e55d
mat43 <mat43@users.noreply.github.com>
2024-07-21 20:22:07 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./**/*.{html,js}",
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        backgroundImage: {
+          'header': "url('/assets/bg.jpg')",
+          'secondary': "url('/assets/secondary.jpg')",
+          'first': "url('/assets/install.jpg')",
+          'second': "url('/assets/second.jpg')",
+          'third': "url('/assets/bg-third.png')",
+        },
+        colors: {
+          primary: {
+            50: 'rgb(214, 19, 19)',
+            100: 'rgb(141, 16, 16)',
+            200: '#bf3030'
+          },
+          secondary: {
+            50: 'rgb(107, 185, 255)',
+          }
+        },
+      },
+      fontFamily: {
+        headings: [
+          'Libre Franklin',
+        ],
+        body: [
+          'Verdana',
+        ],
+        subheadings: [
+          'Varela',
+          'sans-serif'
+        ],
+      },
+    },
+  }
+  
